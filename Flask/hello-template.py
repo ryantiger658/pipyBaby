58d3eb3aaf28411a793768e370b56fe0462a45b9
Ryan Bolyard <tiger658@gmail.com>
2016-12-10 20:22:32 +0000

    Camera Takes a picture and then displays it

--- Flask/hello-template.py
+++ Flask/hello-template.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import datetime
+from time import sleep
+from picamera import PiCamera
+
 app = Flask(__name__)
+# camera = PiCamera()
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.vflip = True
 
 @app.route("/")
 def hello():
@@ -12,9 +19,20 @@ def hello():
 	}
 	return render_template('main.html', **templateData)
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route("/camera/")
 def index_camera():
 	"""Video Streaming Home Page"""
+	# camera = PiCamera()
+	camera.resolution = (1024, 768)
+	camera.vflip = True
+	# sleep(2)
+	camera.capture('static/foo.jpg')
 	return render_template('camera.html')
 
 
