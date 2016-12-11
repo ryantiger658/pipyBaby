1fe7a3bd3e1952bcc5998107507a931c0f05878d
Ryan Bolyard <tiger658@gmail.com>
2016-12-11 02:22:41 +0000

    Adding Initial GPIO Support

--- PiPyBaby.py
+++ PiPyBaby.py
@@ -2,8 +2,11 @@
 from flask import Flask, render_template, Response
 from camera_pi import Camera
 from picamera import PiCamera
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
 
 @app.route('/')
 def index():
@@ -22,13 +25,26 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     """Direct URL of the video stream"""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route("/readPin/<pin>")
+def readPin(pin):
+
+    GPIO.setup(int(pin), GPIO.OUT)
+    if GPIO.input(int(pin)) == True:
+        response = "Pin number " + pin + " is high!"
+        GPIO.output(int(pin), GPIO.LOW)
+    else:
+        response = "Pin number " + pin + " is low!"
+        GPIO.output(int(pin), GPIO.HIGH)
 
 
-@app.route('/fullscreen')
-def fullScreen():
-    return render_template('fullscreen.html')
+    templateData = {
+    'title' : 'Status of pin ' + pin,
+    'response' : response
+    }
+    return render_template('pin.html', **templateData)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
